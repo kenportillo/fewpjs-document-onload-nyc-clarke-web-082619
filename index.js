de64bb26d921af330866c02b0e132c2b455cd098
github username <github email address>
2019-10-08 15:00:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// import { resolveTxt } from "dns"
+
 // Your code goes here
+
+// function changeText(){
+//     document.getElementById('text').innerHTML = 'This is really cool!';
+// }
+
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('text').innerHTML = 'This is really cool!'
+});
